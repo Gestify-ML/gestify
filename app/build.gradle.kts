b4d2408ce6f120af08d2cd32d9b4583aa1275921
Haley <hgslaney@wpi.edu>
2025-02-15 16:47:39 -0500

    updated file path to be relative

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,7 +65,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     //noinspection GradlePath
-    implementation(files("C:/Users/haley/AndroidStudioProjects/gestify/app/spotify-app-remote-release-0.8.0.aar"))
+    implementation(files("spotify-app-remote-release-0.8.0.aar"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
